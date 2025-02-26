a0a60fedb3d62d1cecdb640e0e419d1857b2bc8d
Ronald Tellez <ronaldtellwz@gmail.com>
2025-02-26 12:47:29 -0600

    update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
+    name: 'ProActiva',
+    short_name: 'ProActiva',
+    description: 'Sistema de Gestión Estudiantil',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
